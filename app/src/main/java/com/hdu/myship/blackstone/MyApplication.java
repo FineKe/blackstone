9e272e6b9eca05c90c025e514b7e793a7c1b11ff
kefan <530362991@qq.com>
2017-06-08 17:32:55 +0800

    集成友盟pushsdk

--- app/src/main/java/com/hdu/myship/blackstone/MyApplication.java
+++ app/src/main/java/com/hdu/myship/blackstone/MyApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.content.Context;
 
 import com.beardedhen.androidbootstrap.TypefaceProvider;
+import com.umeng.message.IUmengRegisterCallback;
+import com.umeng.message.PushAgent;
 
 import org.litepal.LitePalApplication;
 import org.litepal.util.Const;
@@ -20,6 +22,22 @@ public class MyApplication extends Application {
         context=getApplicationContext();
         LitePalApplication.initialize(context);
         TypefaceProvider.registerDefaultIconSets();
+
+        PushAgent mPushAgent = PushAgent.getInstance(this);
+//注册推送服务，每次调用register方法都会回调该接口
+        mPushAgent.register(new IUmengRegisterCallback() {
+
+            @Override
+            public void onSuccess(String deviceToken) {
+                //注册成功会返回device token
+            }
+
+            @Override
+            public void onFailure(String s, String s1) {
+
+            }
+        });
+
     }
     public static Context getContext()
     {
